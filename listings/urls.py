72e7c07dd18dfe6255015d2f4582213b99f29aa5
Apollos Gande <60118118+successgande1@users.noreply.github.com>
2021-06-08 15:29:14 +0100

    Updated the Listings and Realtors app and created Listings templates with links

--- listings/urls.py
+++ listings/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 #Create url patterns
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('about', views.about, name = 'about'),
+    path('', views.index, name = 'listings'),
+    path('<int:listing_id>', views.listing, name = 'listing'),
+     path('search', views.search, name = 'search'),
 ]
--- property/settings.py
+++ property/settings.py
@@ -24,6 +24,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,8 +70,11 @@ WSGI_APPLICATION = 'property.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'property',
+        'USER':'successgande',
+        'PASSWORD':'Iam4Jesus',
+        'HOST':'localhost'
     }
 }
 
